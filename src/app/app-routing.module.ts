9c85a6286bad25e6a8f664d961c82440979c73cf
Wai Phyo Han <waiphyohn@gmail.com>
2022-05-27 22:48:31 +0630

    setup Aquila theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./artwork/artwork.module').then(m => m.ArtworkModule) },
   { path: 'module-one', loadChildren: () => import('./module-one/module-one.module').then(m => m.ModuleOneModule) }, 
-  { path: 'module-two', loadChildren: () => import('./module-two/module-two.module').then(m => m.ModuleTwoModule) }, 
-  { path: 'artwork', loadChildren: () => import('./artwork/artwork.module').then(m => m.ArtworkModule) }
+  { path: 'module-two', loadChildren: () => import('./module-two/module-two.module').then(m => m.ModuleTwoModule) }
 ];
 
 @NgModule({
